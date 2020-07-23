4fd19df45507eeb8be7268e519b399091e78bb92
Shi Jie Teo <teoshijie@gmail.com>
2020-07-23 18:59:59 +0800

    Updates

---
+++ precache-manifest.cf5b792979dd0b3d9e0cf4000f42ff35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522d7e142dc2381800528a210b074ce9",
+    "url": "/jservice/index.html"
+  },
+  {
+    "revision": "5980b11861b1646af6f8",
+    "url": "/jservice/static/css/main.cdd42943.chunk.css"
+  },
+  {
+    "revision": "823284abe5f38415899d",
+    "url": "/jservice/static/js/2.8550ff1a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/jservice/static/js/2.8550ff1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5980b11861b1646af6f8",
+    "url": "/jservice/static/js/main.8a7ef936.chunk.js"
+  },
+  {
+    "revision": "fe701ff99de3c25a33ca",
+    "url": "/jservice/static/js/runtime-main.7fc3415f.js"
+  }
+]);
